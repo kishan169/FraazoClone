51330928c8ef306414472f2febfe02477bf2e285
Adarsh <adkhatri250@gmail.com>
2022-05-04 14:23:44 +0530

    adarsh created data

---
+++ adarsh.js
@@ -0,0 +1,573 @@
+var mangoes =[
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/329/original/data?1616795305",
+        name:"Alphonso Mango Devgad",
+        quantity:6,
+        price:479,
+        strike_price:600
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/135/original/data?1612689931",
+        name:"Mango Badami",
+        quantity:1,
+        price:129,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/images/images/000/004/597/original/data?1647577454",
+        name:"Mango Totapuri Raw",
+        quantity:1,
+        price:110,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/250/original/data?1621065378",
+        name:"Mango Lalbhag Sindhura",
+        quantity:1,
+        price:110,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VMAN12.png",
+        name:"Mango Raw",
+        quantity:250,
+        price:14,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FKES1.png",
+        name:"Mango Kesar",
+        quantity:1,
+        price:209,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/339/original/data?1620237313",
+        quantity:1,
+        price:949,
+        strike_price:1200
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/images/images/000/007/294/original/data?1650906368",
+        quantity:1,
+        price:167,
+        strike_price:""
+    }
+    
+]
+
+var Apples = [
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/006/original/data?1642616267",
+        name:"Apple Royal Gala",
+        quantity:4,
+        price:99,
+        strike_price:149
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/244/original/data?1613050070",
+        name:"Wood Apple",
+        quantity:1,
+        price:18,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/096/original/data?1625415915",
+        name:"Pear Green / Packham",
+        quantity:500,
+        price:122,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/241/original/data?1610967581",
+        name:"Apple Red Delicious",
+        quantity:4,
+        price:129,
+        strike_price:179
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FAPP23.png",
+        name:"Apple Green / Granny Smith",
+        quantity:4,
+        price:243,
+        strike_price:""
+    }
+]
+var oranges = [
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FMOS13.png",
+        name:"Mosambi(Sweet Melon)",
+        quantity:1,
+        price:122,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FCOC1.png",
+        name:"Coconut",
+        quantity:1,
+        price:22,
+        strike_price:31
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FMUS1.png",
+        name:"Musk Melon/ Kharabooja",
+        quantity:1,
+        price:39,
+        strike_price:62
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/117/original/data?1616765390",
+        name:"Watermelon",
+        quantity:1,
+        price:29,
+        strike_price:179
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/338/original/data?1620150734",
+        name:"Orange Imported",
+        quantity:2,
+        price:38,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/257/original/data?1612448669",
+        name:"Coconut Tender / Nariyal Pani",
+        quantity:1,
+        price:49,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/images/images/000/004/598/original/data?1647718256",
+        name:"Special Offer Watermelon",
+        quantity:1,
+        price:9,
+        strike_price:"44"
+    },
+]
+
+var papaya = [
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VPAP12.png",
+        name:"Papaya Raw",
+        quantity:1,
+        price:28,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/157/original/data?1595448197",
+        name:"Pineapple",
+        quantity:1,
+        price:800,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/261/original/data?1612541635",
+        name:"Pomegranate(Anar)",
+        quantity:3,
+        price:159,
+        strike_price:169
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/092/original/data?1620114635",
+        name:"Papaya",
+        quantity:1,
+        price:54,
+        strike_price:""
+    }
+]
+
+var grapes = [
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FBAN14.png",
+        name:"Banana Robusta",
+        quantity:1,
+        price:41,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FBAN24.png",
+        name:"Banana Elaichi/Yellaki",
+        quantity:500,
+        price:52,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FCHI13.png",
+        name:"Chikoo",
+        quantity:500,
+        price:38,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/369/original/data?1622631944",
+        name:"Guava White Indian",
+        quantity:2,
+        price:79,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/260/original/data?1612450264",
+        name:"Banana Raw",
+        quantity:500,
+        price:22,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/248/original/data?1612379941",
+        name:"Banana Red",
+        quantity:500,
+        price:78,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FGRA42.png",
+        name:"Grapes Green",
+        quantity:500,
+        price:66,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/247/original/data?1612371028",
+        name:"Banana Nendran",
+        quantity:500,
+        price:79,
+        strike_price:""
+    }
+]
+
+var Exotic  = [
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/432/original/data?1629889583",
+        name:"Grape Fruit",
+        quantity:1,
+        price:105,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FAVO21.png",
+        name:"Avocado Indian",
+        quantity:1,
+        price:246,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FBLU1.png",
+        name:"Blueberry",
+        quantity:1,
+        price:319,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FDRA1.png",
+        name:"Dragon Fruit",
+        quantity:1,
+        price:75,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FKIW2.png",
+        name:"Kiwi Green",
+        quantity:1,
+        price:100,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/235/original/data?1610033950",
+        name:"Avocado Imported",
+        quantity:1,
+        price:290,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FANJ2.png",
+        name:"Anjeer(Figs)",
+        quantity:1,
+        price:78,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/243/original/data?1611939790",
+        name:"Mulberry",
+        quantity:1,
+        price:108,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/388/original/data?1624798076",
+        name:"Plum Imported",
+        quantity:6,
+        price:238,
+        strike_price:""
+    }
+]
+
+var fruitcombos  = [
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/442/original/data?1635063552",
+        name:"Smoothie Combo (Banana Robusta - 1Kg + Blueberry - 1 Punnet)",
+        quantity:1,
+        price:357,
+        strike_price:360
+    },
+    {
+        img:"hhttps://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/240/original/data?1642616123",
+        name:"Tasty Combo (2 Pcs Apple Royal Gala + 1 Kg Banana Robusta)",
+        quantity:1,
+        price:112,
+        strike_price:115
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/204/original/data?1621240725",
+        name:"Cooler Fruit Combo (1 Pc Each Watermelon + Papaya)",
+        quantity:1,
+        price:84,
+        strike_price:87
+    }
+]
+
+var dailyveggies  = [
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VBRI12.png",
+        name:"Brinjal Bharta",
+        quantity:500,
+        price:24,
+        strike_price:360
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/101/original/data?1612451098",
+        name:"Pumpkin Green",
+        quantity:500,
+        price:16,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VCAP12.png",
+        name:"Capsicum (Shimla), Green Shimla Mirchi",
+        quantity:250,
+        price:13,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VCAP12.png",
+        name:"Capsicum (Shimla), Green Shimla Mirchi",
+        quantity:250,
+        price:13,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/315/original/data?1614700255",
+        name:"Cucumber Green Indian / Kheera / Kakdi",
+        quantity:500,
+        price:22,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VCAB12.png",
+        name:"Cabbage",
+        quantity:1,
+        price:9,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VCAU12.png",
+        name:"Cauliflower",
+        quantity:1,
+        price:38,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VBIT12.png",
+        name:"Bitter Gourd (Karela)",
+        quantity:500,
+        price:23,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VBOT12.png",
+        name:"Bottle Gourd (Dudhi/Lauki)",
+        quantity:1,
+        price:9,
+        strike_price:12
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/028/original/data?1616765740",
+        name:"Brinjal Kateri, Baingan",
+        quantity:250,
+        price:10,
+        strike_price:15
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VDRU12.png",
+        name:"Drumstick",
+        quantity:100,
+        price:4,
+        strike_price:6
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/106/original/data?1603884029",
+        name:"Snake Gourd (Bada Parwal)",
+        quantity:250,
+        price:16,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/316/original/data?1614700848",
+        name:"Cucumber White",
+        quantity:500,
+        price:20,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VBRI22.png",
+        name:"Brinjal Black/Nagpur",
+        quantity:500,
+        price:17,
+        strike_price:24
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/images/images/000/006/625/original/data?1649244316",
+        name:"Ridge Gourd (Turai) 500 g",
+        quantity:500,
+        price:26,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VGAV12.png",
+        name:"Beans Cluster (Gavar)",
+        quantity:250,
+        price:12,
+        strike_price:16
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VTON12.png",
+        name:"Tondli",
+        quantity:500,
+        price:33,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VFRE12.png",
+        name:"Beans French",
+        quantity:500,
+        price:108,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VTIN12.png",
+        name:"Tinda",
+        quantity:250,
+        price:27,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/images/images/000/006/624/original/data?1649243792",
+        name:"Lady's Finger (Bhindi / Okra / Bhendi)",
+        quantity:500,
+        price:17,
+        strike_price:20
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VPUM12.png",
+        name:"Pumpkin Red",
+        quantity:500,
+        price:23,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/164/original/data?1629975839",
+        name:"Ash Gourd (White Pumpkin)",
+        quantity:500,
+        price:29,
+        strike_price:""
+    }
+]
+
+var onion  = [
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/images/images/000/006/605/original/data?1649242645",
+        name:"Tomato (1 kg)",
+        quantity:1,
+        price:42,
+        strike_price:45
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/BONI11.png",
+        name:"Onion",
+        quantity:1,
+        price:16,
+        strike_price:20
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/BPOT11.png",
+        name:"Potato/Batata",
+        quantity:1,
+        price:27,
+        strike_price:29
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/236/original/data?1610137718",
+        name:"Tomato-Cherry",
+        quantity:200,
+        price:21,
+        strike_price:""
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/234/original/data?1610025881",
+        name:"Onion - Sambhar",
+        quantity:250,
+        price:20,
+        strike_price:""
+    }
+]
+
+var Root  = [
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VBEE12.png",
+        name:"Beetroot",
+        quantity:500,
+        price:20,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VARB12.png",
+        name:"Arbi(Colocasia)",
+        quantity:500,
+        price:35,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VSUR12.png",
+        name:"Yam (Suram/Elephant Foot)",
+        quantity:500,
+        price:35,
+        strike_price:""
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FCAR1.png",
+        name:"Carrot Orange (Local)",
+        quantity:500,
+        price:22,
+        strike_price:28
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VMUL12.png",
+        name:"Radish",
+        quantity:500,
+        price:25,
+        strike_price:28
+    },
+    {
+        img:"https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/246/original/data?1631690575",
+        name:"Yam Purple(Kandh)",
+        quantity:250,
+        price:25,
+        strike_price:28
+    },
+    {
+        img:"https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VSWE12.png",
+        name:"Sweet Potato",
+        quantity:500,
+        price:24,
+        strike_price:""
+    }
+]
